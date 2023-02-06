7563e4ab5d553f0d2c69013e592c404a694508d1
dongl1n <102480011+dongl1n@users.noreply.github.com>
2023-02-06 17:46:22 +0300

    feat: add func

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-
-// You should implement your task here.
+function towelSort(arr){
+  let towel=[];
+  for(key in arr)
+  for(key2 in arr[key]) towel.push(arr[key][key2]);
+  return towel;
+}
 
 module.exports = function towelSort (matrix) {
   return [];
